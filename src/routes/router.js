0327e581dd2be1099a9b53d9877181a2073ccd61
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-04-19 12:01:29 -0300

    remove: project files

--- src/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const timer = require("../controllers/timerController");
-
-const router = express();
-
-router.get("/timer", timer.getTimers);
-router.post("/timer", timer.postTimer);
-
-module.exports = router;
